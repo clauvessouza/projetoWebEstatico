c5e63b5d2669092828148d60aa17fcb65246a10f
clauvessouza <claudio.alves@estudante.iftm.com.br>
2024-10-04 23:53:28 -0300

    novo commit

--- JS_lista_02 modificado/exer01- array/js/script.js
+++
@@ -1,4 +0,0 @@
-const dataAtual = new Date();
-var diasSemana = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado"];
-
-document.write(`Hoje é ${diasSemana[dataAtual.getDay()]}`);
